fecc00bd944a40af0a972530517c4f7b38af69bd
Aka Nawelfe <nawel.chaoui@laplateforme.io>
2023-05-02 12:43:06 +0200

    ajout job11

---
+++ jour01/job11/job11.sql
@@ -0,0 +1,7 @@
+mysql> SELECT * FROM etudiants WHERE nom = 'Dupuis';
++----+--------+----------+-----+---------------------------------+
+| id | nom    | prenom   | age | email                           |
++----+--------+----------+-----+---------------------------------+
+|  5 | Dupuis | Gertrude |  20 | gertrude.dupuis@laplateforme.io |
++----+--------+----------+-----+---------------------------------+
+1 row in set (0.00 sec)
