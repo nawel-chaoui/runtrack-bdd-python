d22ddc6ff6d3f45203218a96d406cc7a4d7d37a9
Aka Nawelfe <nawel.chaoui@laplateforme.io>
2023-05-03 10:43:29 +0200

    add job04

---
+++ jour02/job04.py
@@ -0,0 +1,25 @@
+import mysql.connector
+
+# Se connecter à la base de données
+db = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="laPlateformedeoz!1702",
+  database="LaPlateforme"
+)
+
+# Créer un curseur
+cursor = db.cursor()
+
+# Exécuter une requête SELECT pour récupérer les noms et les capacités de la table "salles"
+query = "SELECT nom, capacite FROM salles"
+cursor.execute(query)  # "query" est un terme utilisé dans le domaine des bases de données pour désigner une demande
+                       # d'information ou une requête envoyée à une base de données afin de récupérer des données
+
+# Récupérer les résultats et les afficher en console
+for nom, capacite in cursor:
+    print(f"\nNom: {nom}\nCapacité: {capacite}")
+
+# Fermer le curseur et la connexion
+cursor.close()
+db.close()
